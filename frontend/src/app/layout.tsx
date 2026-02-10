b018a2f97535962a1c16657404140513c5ad9b04
Duy_Beo <duy171534@gmail.com>
2026-02-10 16:37:50 +0700

    V-Public-1.0

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Nunito } from "next/font/google";
 import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
+import Script from "next/script"
 
 const nunito = Nunito({
   subsets: ["latin", "vietnamese"],
@@ -44,6 +45,15 @@ export default function RootLayout({
       <body className={`${nunito.variable} font-sans antialiased`}>
         {children}
         <Analytics />
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "vf4jsn9isy");
+          `}
+        </Script>
       </body>
     </html>
   );
